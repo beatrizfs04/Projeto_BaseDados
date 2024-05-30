bbf9d0ea69f6c14963287b651ba152c624baf3c7
beatrizfs04 <beatrizfs2004@gmail.com>
2024-05-30 15:42:49 +0100

    Controllers

---
+++ Controllers/routers.js
@@ -0,0 +1,29 @@
+/* 
+queries.update = async function() {
+    db.query('UPDATE tabela SET objeto = ? WHERE campo = ?', (error, results) => {
+        if (error) throw error; 
+        res.send(results);
+    })
+}
+
+queries.select = async function() {
+    db.query('SELECT objetos FROM tabela WHERE campo = ?', (error, results) => {
+        if (error) throw error; 
+        res.send(results);
+    })
+}
+
+queries.insert = async function() {
+    db.query('INSERT INTO tabela (campos, campos, campos) VALUES (objeto, objeto, objeto)', (error, results) => {
+        if (error) throw error; 
+        res.send(results);
+    })
+}
+
+queries.delete = async function() {
+    db.query('DELETE FROM tabela WHERE campo = ?', (error, results) => {
+        if (error) throw error; 
+        res.send(results);
+    })
+} 
+*/
--- index.js
+++ index.js
@@ -8,34 +8,6 @@ const queries = express.Router();
 const port = 3000;
 const pageTitle = "Projeto Final - Base de Dados";
 
-/* queries.update = async function() {
-    db.query('UPDATE tabela SET objeto = ? WHERE campo = ?', (error, results) => {
-        if (error) throw error; 
-        res.send(results);
-    })
-}
-
-queries.select = async function() {
-    db.query('SELECT objetos FROM tabela WHERE campo = ?', (error, results) => {
-        if (error) throw error; 
-        res.send(results);
-    })
-}
-
-queries.insert = async function() {
-    db.query('INSERT INTO tabela (campos, campos, campos) VALUES (objeto, objeto, objeto)', (error, results) => {
-        if (error) throw error; 
-        res.send(results);
-    })
-}
-
-queries.delete = async function() {
-    db.query('DELETE FROM tabela WHERE campo = ?', (error, results) => {
-        if (error) throw error; 
-        res.send(results);
-    })
-} */
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
